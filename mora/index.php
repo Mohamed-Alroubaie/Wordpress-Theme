8c33cd4d135e627e066f5cfc59f2e551afea0d8e
Mohamed-Alroubaie <65648087+Mohamed-Alroubaie@users.noreply.github.com>
2022-07-03 21:10:25 +0200

    Updating index.php

--- mora/index.php
+++ mora/index.php
@@ -1,78 +1,47 @@
 <?php get_header(); ?>
-<div class="sidebar_container">       
-		<div class="sidebar">
-          <div class="sidebar_item">
-            <h2>New Website</h2>
-            <p>Welcome to our new website. Please have a look around, any feedback is much appreciated.</p>
-          </div><!--close sidebar_item--> 
-        </div><!--close sidebar-->     		
-		<div class="sidebar">
-          <div class="sidebar_item">
-            <h2>Latest Update</h2>
-            <h3>November 2012</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim.</p>         
-		  </div><!--close sidebar_item--> 
-        </div><!--close sidebar-->
-		<div class="sidebar">
-          <div class="sidebar_item">
-            <h3>November 2012</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim.</p>         
-		  </div><!--close sidebar_item--> 
-        </div><!--close sidebar-->  		
-        <div class="sidebar">
-          <div class="sidebar_item">
-            <h2>Contact</h2>
-            <p>Phone: +44 (0)1234 567891</p>
-            <p>Email: <a href="mailto:info@youremail.co.uk">info@youremail.co.uk</a></p>
-          </div><!--close sidebar_item--> 
-        </div><!--close sidebar-->
-       </div><!--close sidebar_container-->	
-
-      <div class="slideshow">
-	    <ul class="slideshow">
-          <li class="show"><img width="680" height="250" src="images/home_1.jpg" alt="&quot;Enter your caption here&quot;" /></li>
-          <li><img width="680" height="250" src="images/home_2.jpg" alt="&quot;Enter your caption here&quot;" /></li>
-        </ul> 
-	  </div>
-	   
-	  <div id="content">
-        <div class="content_item">
-		  <h1>Welcome To Your Website</h1> 
-          <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under the Creative Commons Attribution 3.0 Licence), which means that you are free to download and use it for anything you want (including modifying and amending it). If you would like to remove the &lsquo;website template by Free HTML5 Templates&rsquo; link from the footer of a template / website then that is OK, providing you make a small donation via the website. I do not specify an amount, just what you think the design is worth.</p>   		
-		  
-		  <div class="content_imagetext">
-		    <div class="content_image">
-		      <img src="images/image1.jpg" alt="image1"/>
-	        </div>
-		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elit sapien, tempus sit amet hendrerit volutpat, euismod vitae risus. Etiam consequat, sem et vulputate dapibus, diam enim tristique est, vitae porta eros mauris ut orci. Praesent sed velit odio. Ut massa arcu, suscipit viverra molestie at, aliquet a metus. Nullam sit amet tellus dui, ut tincidunt justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis egestas laoreet. Nunc non ipsum metus, non laoreet urna. Vestibulum quis risus quis diam mattis tempus. Vestibulum suscipit pretium tempor. </p>
-		  </div><!--close content_imagetext-->
-		  
-		  <div class="content_container">
-		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>
-		  	<div class="button_small">
-		      <a href="#">Read more</a>
-		    </div><!--close button_small-->
-		  </div><!--close content_container-->
-          <div class="content_container">
-		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>          
-		  	<div class="button_small">
-		      <a href="#">Read more</a>
-		    </div><!--close button_small-->		  
-		  </div><!--close content_container-->			  
-		</div><!--close content_item-->
-      </div><!--close content-->   
-	</div><!--close site_content-->  	
-
-    <footer>
-	  <a href="index.html">Home</a> | <a href="ourwork.html">Our Work</a> | <a href="testimonials.html">Testimonials</a> | <a href="projects.html">Projects</a> | <a href="contact.html">Contact</a><br/><br/>
-	  website template by <a href="http://www.freehtml5templates.co.uk">Free HTML5 Templates</a>
-    </footer>
-	
-  </div><!--close main-->
-  
-</body>
-</html>
-
-
 <?php get_sidebar();?>
+
+
+
+<div id="content">
+  <div class="content_item">
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : ?>
+      <?php the_post(); ?>
+      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <section class="box info">
+    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent link to this one article">
+    <?php the_title('<h2>', '</h2>', true); ?>
+  </a>
+  <div>
+    <?php comments_popup_link('No comment', '1 comment', '% Comments', '', 'Comments are disabled.'); ?>
+    <?php edit_post_link('Edit',' | ',''); ?>
+  </div>
+  <?php the_content('<br /><span class="ym-button ym-next">Read more</span>'); ?>
+  <div>
+    <?php comments_template(); ?>
+  </div>
+  <p>Written by <b><?php the_author(); ?></b> am <?php the_time('j.F Y'); ?> um <?php the_time('G:i'); ?> clock, filed under <?php the_category(' | '); ?></p>
+</section>
+</div>
+<?php endwhile; ?>
+<p>
+  <?php previous_post_link('« %link') ?>
+  <?php next_post_link('<span style="float:right">%link »</span>') ?>
+</p>
+<?php else : ?>
+  <h2>Nothing found</h2>
+  <p>Sorry, but you are looking for something that is not here.</p>
+  <?php endif; ?>
+  <!--close button_small-->
+</div>
+<!--close content_container-->
+</div>
+<!--close content_item-->
+</div>
+<!--close content-->
+</div>
+<!--close site_content-->
+<!-- bis hei Content -->
+
 <?php get_footer(); ?>
